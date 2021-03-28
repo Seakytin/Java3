dfc46b6197a8173361c2c64e1edde6cb4994cc84
Seakytin <Seakytin@yandex.ru>
2021-03-28 11:16:14 +0300

    lesson7

--- src/main/java/lesson6/ArithmeticAction.java
+++ src/main/java/lesson6/ArithmeticAction.java
@@ -23,8 +23,27 @@ public class ArithmeticAction {
     public int arrayElementSum(int [] arr) {
         return arr[0] + arr[2];
     }
-    public int arrayElement(int [] arr) {
-        return arr[0] + arr[2];
+    public int arrayElementAfter(int [] arr) {
+        int x = 0;
+        int[] result = null;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 4) {
+                x = arr[i];
+                result = new int[arr.length - i - 1];
+                for (int j = i + 1, k = 0; j < arr.length; j++, k++) {
+                    if (arr[j] == 4) continue;
+                    result[k] = arr[j];
+                    //System.out.println(result);
+                }
+            }
+        }
+        try {
+            x = 1 / x;
+        } catch (ArithmeticException e) {
+            throw new RuntimeException();
+        }
+//return result;
+        return x;
     }
 
     public TreeSet<String> sort(String ... str) {
